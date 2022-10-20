a5f3da44a6bf1c9d21b4a987ff270bdd6072a83d
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2022-10-20 21:24:22 +0200

    Drawer für Form

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,6 @@ import Navbar from './Navbar';
 
 // TODO gleich Router einbauen?
 // TODO Aufteilung Spalten auf mobile 50/50 horiz/vert umsetzen
-// TODO Form overlay umsetzen
 function App() {
   const [date, setDate] = useState(new Date());
   const [viewMode, setViewMode] = useState(ViewMode.MONTH_DISPLAY);
@@ -18,15 +17,20 @@ function App() {
         viewMode={viewMode} setViewMode={setViewMode} />
       <div className="container overflow-hidden">
         <div className="row h-100">
-          <div id="main" className='col col-8 h-100 overflow-auto'>
+          <div id="main" className='col col-8 h-100 overflow-auto pt-2'>
             <Blindtext numParagraphs={10}></Blindtext>
           </div>
-          <div id="side" className='col col-4 h-100 overflow-auto'>
-            <DayExpenses date={date} />
+          <div id="side" className='col col-4 h-100 overflow-hidden pe-0 position-relative'>
+            <div className="overflow-auto w-100 h-100 pt-2">
+              <DayExpenses date={date} />
+            </div>
+            <div id="drawer-right" className="position-absolute top-0 start-100 overflow-auto w-100 h-100 bg-white ps-2 pt-2">
+              <Blindtext numParagraphs={4} />
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
