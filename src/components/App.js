897639e8f17971b99a91d9c23dca55a4b98dedee
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2022-10-28 20:25:35 +0200

    Drawer geht auch wenn side unsichtbar ist

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,6 @@ import PositionOutline from './PositionOutline.js';
 import React from 'react';
 
 // TODO gleich Router einbauen?
-// TODO Aufteilung Spalten auf mobile 50/50 horiz/vert umsetzen/allgemein mobile (Drawer anschauen)
-// TODO rounded corners für suspense
 function App() {
   return (
     <PositionOutline />
--- src/components/Outline.js
+++ src/components/Outline.js
@@ -6,6 +6,7 @@ import Navbar from './Navbar';
 import TagContext from './TagContext.js';
 import { getTagsAndHierarchy } from '../services/TagService.js';
 
+// FIXME shift6m quer ist sm, nicht md!
 // FIXME re-add viewmode changing when adding sidebar
 export default function Outline(props) {
     const [viewMode, setViewMode] = useState(ViewMode.MONTH_DISPLAY);
@@ -17,10 +18,9 @@ export default function Outline(props) {
 
     const drawerPosStart = props.rightDrawerVisible ? 'start-0' : 'start-100';
     const mainClasses = props.sideOnMobile ? 'h-50' : 'h-100';
-    const sideClasses = props.sideOnMobile ? '' : 'd-none d-md-block';
+    const sideClasses = props.sideOnMobile ? 'h-50' : 'h-0';
 
     // FIXME Inhalt des Drawers behalten, bis der aus dem Bild verschwunden ist
-    // FIXME Drawer wird nicht angezeigt, wenn side nicht angezeigt wird
     return (
         <div id="app" className='d-flex h-100 flex-column overflow-hidden position-relative'>
             <Navbar viewMode={viewMode} setViewMode={setViewMode}>
@@ -32,7 +32,7 @@ export default function Outline(props) {
                         <div id="main" className={`col-md col-lg-8 h-md-100 overflow-auto pt-2 ${mainClasses}`}>
                             {props.main}
                         </div>
-                        <div id="side" className={`col-md col-lg-4 h-50 h-md-100 px-0 ${sideClasses} position-md-relative`}>
+                        <div id="side" className={`col-md col-lg-4 h-md-100 px-0 ${sideClasses} position-md-relative`}>
                             <div className="overflow-auto pt-2 h-100 gutter-padding">
                                 {props.side}
                             </div>
