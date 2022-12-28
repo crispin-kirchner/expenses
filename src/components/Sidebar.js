4e0eb71a1c89a35611c02256d2615eeb9ac09fd7
Crispin Kirchner <crispin.kirchner@bsi-software.com>
2022-12-28 17:36:51 +0100

    Hamburger für Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,5 @@
+// TODO nen burger machen mit nem Pfeil
+import { Squash as Hamburger } from 'hamburger-react';
 import t from "../utils/texts";
 import { useState } from "react";
 
@@ -23,18 +25,20 @@ function Section({ caption, children }) {
 }
 
 export default function Sidebar({ children }) {
-  const [sidebarCollapse, setSidebarCollapse] = useState(true);
+  const [isCollapsed, setCollapsed] = useState(true);
 
   return (
-    <div id="sidebar" className={`position-absolute p-2 h-100 d-flex flex-column flex-shrink-0 bg-light ${sidebarCollapse ? 'collapsed' : ''}`}>
-      <span className="d-none d-xl-block">
-        <i className="bi bi-piggy-bank align-middle" style={{ fontSize: '2rem' }}></i>
-        <span className='sidebar-brand align-middle ms-2'>{t('Expenses')}</span>
+    <div id="sidebar" className={`position-absolute p-2 h-100 d-flex flex-column flex-shrink-0 bg-light ${isCollapsed ? 'collapsed' : ''}`}>
+      <span className="d-none d-xl-flex align-items-center text-center">
+        <div style={{ width: '48px' }}>
+          <i className="bi bi-piggy-bank" style={{ fontSize: '2rem' }}></i>
+        </div>
+        <span className='sidebar-brand ms-2'>{t('Expenses')}</span>
       </span>
-      <a href="#" onClick={() => setSidebarCollapse(sc => !sc)} className="d-xl-none text-nowrap align-middle link-dark text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="right">
-        <i className='bi bi-list align-middle' style={{ fontSize: '2rem' }}></i>
-        <span className="ms-2 collapsed-hidden align-middle sidebar-brand">{t('Expenses')}</span>
-      </a>
+      <span className="d-flex align-items-center d-xl-none">
+        <Hamburger toggled={!isCollapsed} toggle={() => setCollapsed(c => !c)} />
+        <span className="ms-2 collapsed-hidden sidebar-brand">{t('Expenses')}</span>
+      </span>
       <hr className='mt-2' />
       {children}
     </div>
