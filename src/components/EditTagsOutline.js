ffe9bc00a5eb5e6c5e34f0e5c9da3404bfe57bdf
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2024-01-04 20:49:47 +0100

    Render tag hierarchy in EditTagsOutline

--- src/components/EditTagsOutline.js
+++ src/components/EditTagsOutline.js
@@ -1,8 +1,44 @@
 import Outline from "./Outline";
+import TagDimension from "../enums/TagDimension";
+import Tags from "./Tags";
 import t from "../utils/texts";
+import { useContext } from "react";
 
-// TODO implement
-export default function EditTagsOutline({unsyncedDocuments}) {
+function TagLine({ tag, subHierarchy }) {
+  let children = null;
+  if (subHierarchy) {
+    children = <TagHierarchy tags={subHierarchy} />
+  }
+  return <li>
+    {tag}
+    {children}
+  </li>
+}
+
+function TagHierarchy({ tags }) {
+  return <ul>
+    {Object.entries(tags).map(([k, v]) => <TagLine key={k} tag={k} subHierarchy={v} />)}
+  </ul>;
+}
+
+function TagsInternal() {
+  // Helper component, because the Tags context is only defined inside the outline
+  const tags = useContext(Tags.Context);
+  if (!tags) {
+    return 'FIXME suspense Keine Tags definiert';
+  }
+
+  return Object.values(TagDimension).map(d => <>
+    <h1>{d}</h1>
+    <TagHierarchy tags={tags.hierarchy[d]} />
+  </>);
+}
+
+export default function EditTagsOutline({ isSidebarCollapsed, toggleSidebar, unsyncedDocuments }) {
   return <Outline
-    navbarBrandContent={t('EditTags')}/>
+    isSidebarCollapsed={isSidebarCollapsed}
+    toggleSidebar={toggleSidebar}
+    navbarBrandContent={t('EditTags')}
+    navbarFormContent={unsyncedDocuments}
+    main={<TagsInternal />} />
 }
--- src/enums/TagDimension.js
+++ src/enums/TagDimension.js
@@ -2,8 +2,8 @@ const STANDARD = 'Standard';
 const UNSPECIFIC = 'Andere';
 
 const TagDimension = {
-    STANDARD: STANDARD,
-    UNSPECIFIC: UNSPECIFIC
+  STANDARD: STANDARD,
+  UNSPECIFIC: UNSPECIFIC
 };
 
 export default TagDimension;
