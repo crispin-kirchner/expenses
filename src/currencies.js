572b5200980c69e4f9c3599ba1a3e26d62999f0e
Crispin Kirchner <6740795+crispin-kirchner@users.noreply.github.com>
2024-08-09 11:16:10 +0200

    Währung CZK hinzufügen

--- src/currencies.js
+++ src/currencies.js
@@ -3,6 +3,7 @@ import * as constants from './constants.js';
 const CHF = '302dd3a2-3e84-4b56-ac99-3ddd66b686d7';
 const EUR = 'e190e651-a073-449d-b674-8ececd2f096e';
 const DKK = 'b1475394-2bfb-4993-a2e5-a4a61d761a6c';
+const CZK = '030add7a-a0cd-4682-bb85-e50029f78b23';
 
 const definitions = {};
 
@@ -22,6 +23,12 @@ definitions[DKK] = {
     displayName: 'kr.',
     isoCode: 'DKK',
     id: DKK
+};
+
+definitions[CZK] = {
+    displayName: 'Kč',
+    isoCode: 'CZK',
+    id: CZK
 };
 
 function isDefault(currencyId) {
